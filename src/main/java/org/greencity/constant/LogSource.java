7f7d7be5b7fa997eece5fc7ab8920e8f74f9da23
Maksym Kozak <bersercer70@gmail.com>
2025-03-23 17:51:07 +0200

    rename jobs

--- src/main/java/org/greencity/constant/LogSource.java
+++ src/main/java/org/greencity/constant/LogSource.java
@@ -3,8 +3,8 @@ package org.greencity.constant;
 public enum LogSource {
 
     GREENCITY(EnvVar.GREENCITY_LOGS_URL.value(), "GreenCity"),
-    GREENCITY_UBS(EnvVar.GREENCITY_UBS_LOGS_URL.value(), "GreenCityUBS"),
-    GREENCITY_USER(EnvVar.GREENCITY_USER_LOGS_URL.value(), "GreenCityUser");
+    GREENCITY_UBS(EnvVar.GREENCITY_UBS_LOGS_URL.value(), "GreenCity UBS"),
+    GREENCITY_USER(EnvVar.GREENCITY_USER_LOGS_URL.value(), "GreenCity User");
 
     private final String logsUrl;
     private final String jobName;
